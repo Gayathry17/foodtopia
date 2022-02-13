195acb265c2a8662f35216c3cbb1886986b3570c
Goutham-R <gouthamrejith123@gmail.com>
2022-02-13 22:26:14 +0530

    Profile component modified

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,88 +1,30 @@
-import React, { Component } from "react";
-
-class Profile extends Component {
-  state = {
-    data: [],
-    per: 9,
-    page: 1,
-    total_pages: null
-  };
-
-  uppercase = word => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  };
-
-  loadData = () => {
-    const { per, page, data } = this.state;
-    const endpoint = `https://randomuser.me/api/?nat=us&results=${per}&page=${page}`;
-    fetch(endpoint)
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          data: [...data, ...json.results],
-          scrolling: false,
-          total_pages: json.info.results
-        });
-      });
-  };
-
-  loadMore = () => {
-    this.setState(
-      prevState => ({
-        page: prevState.page + 1,
-        scrolling: true
-      }),
-      this.loadData
-    );
-  };
-
-  componentDidMount() {
-    this.loadData();
-  }
-
-  render() {
-    return (
-      <div className="clearfix">
-        <div className="row">
-          {this.state.data.map(data => (
-            <div className="col-md-4 animated fadeIn" key={data.id.value}>
-              <div className="card">
-                <div className="card-body">
-                  <div className="avatar">
-                    <img
-                      src={data.picture.large}
-                      className="card-img-top"
-                      alt=""
-                    />
-                  </div>
-                  <h5 className="card-title">
-                    {this.uppercase(data.name.first) +
-                      " " +
-                      this.uppercase(data.name.last)}
-                  </h5>
-                  <p className="card-text">
-                    {data.location.city +
-                      ", " +
-                      this.uppercase(data.location.state)}
-                    <br />
-                    <span className="phone">{data.phone}</span>
-                  </p>
-                </div>
-              </div>
+import React from 'react'
+import profile_temp from "./profile_temp.jpeg"
+function Profile() {
+  return (
+    <div className="profileContainer">
+      <h1 className="profile_header">User Profile</h1>
+        <div className="profile_header">
+          <img src={profile_temp} alt="profile_pic" />
+            <div className="profile-name">
+              <h4>Name:</h4>
+              <h3>John Doe</h3>
             </div>
-          ))}
+            <div className="profile-email">
+              <h4>Email:</h4>
+              <h3>johndoe@gmail.com</h3>
+            </div>
+            <div className="profile_username">
+              <h4>Username:</h4>
+              <h3>John__Doe</h3>
+            </div>
+            <div className="profile_phone">
+              <h4>Phone no:</h4>
+              <h3>+91 97461 46820</h3>
+            </div>
         </div>
-        <button
-          className="btn btn-light btn-block w-50 mx-auto"
-          onClick={e => {
-            this.loadMore();
-          }}
-        >
-          Load More Users
-        </button>
-      </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default Profile;
+export default Profile
